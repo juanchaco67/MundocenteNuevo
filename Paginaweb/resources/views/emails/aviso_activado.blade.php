e6c014944a17bbbb84afcc47cace807d11845c8a
juan camilo <nuevojuanchaco67@gmail.com>
2016-07-23 00:29:17 -0500

    ahora si

---
+++ Paginaweb/resources/views/emails/aviso_activado.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+
+<style>
+
+
+</head>
+<body>
+<h2>Hola  {{$user->name}}</h2>
+
+<div>
+    Su cuenta  ha  cambiado al estado: {{ $user->estado }}
+</div>	
+<div>
+<p>
+Cambiaste la configuración recientemente, por lo que tu cuenta de Mundocente <a>{{$user->email}}</a>
+@if($user->estado=="activo")
+	ya está {{$user->estado}}.</br>
+	<a href="http://grupo2.virtualtic.co/" class="myButton">Ir a tu cuenta</a>
+ @else
+	ya no está {{$user->estado}}.
+@endif
+</p>
+</div>
+
+</body>
+</html>
