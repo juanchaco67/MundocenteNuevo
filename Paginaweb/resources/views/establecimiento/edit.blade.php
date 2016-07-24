f1694be6538b2398e3cca1077323498e6ea12b4c
Sergio <sepi_147@hotmail.com>
2016-07-24 18:44:52 -0500

    Corrijo error en establecimiento al editar

--- Paginaweb/resources/views/establecimiento/edit.blade.php
+++ Paginaweb/resources/views/establecimiento/edit.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.paneladmin')
 
 @section('titulo-pagina')
-	Editar publicación
+	Editar establecimiento
 @stop
 
 @section('contenido')
 	@include('alerts.request')
 	<!-- <h1>Editar</h1> -->
-		{!!Form::model($publicacion, ['route'=>['publicacion.update', $publicacion->id], 'method'=>'put'])!!}
-			@include('publicacion.forms.formulario')
-		{!!Form::submit('Actualizar', ['class'=>'btn btn-primary', 'title'=>'Actualizar esta Universidad/Entidad']])!!}
+		{!!Form::model($establecimiento, ['route'=>['establecimiento.update', $establecimiento->id], 'method'=>'put'])!!}
+			@include('establecimiento.forms.formulario')
+		{!!Form::submit('Actualizar', ['class'=>'btn btn-primary', 'title'=>'Actualizar esta Universidad/Entidad'])!!}
 		{!!Form::close()!!}
 
-		{!!Form::open(['route'=>['publicacion.destroy', $publicacion->id], 'method'=>'delete'])!!}
-		{!!Form::submit('Borrar', ['class'=>'btn btn-danger', 'title'=>'Borrar esta Universidad/Entidad']])!!}
+		{!!Form::open(['route'=>['establecimiento.destroy', $establecimiento->id], 'method'=>'delete'])!!}
+		{!!Form::submit('Borrar', ['class'=>'btn btn-danger', 'title'=>'Borrar esta Universidad/Entidad'])!!}
 		{!!Form::close()!!}
 @stop
