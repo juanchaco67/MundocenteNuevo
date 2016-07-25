77788d4d4507dac7f8f1a85ac973c2d8ae7ed29e
Sergio <sepi_147@hotmail.com>
2016-07-25 04:20:39 -0500

    Envio correo de formulario contacto

---
+++ Paginaweb/resources/views/emails/mensaje_contacto.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+
+<style>
+
+
+</head>
+<body>
+<h2>Hola  Admin</h2>
+
+<div>
+    Mensaje de usuario
+</div>	
+<div>
+<p>
+Gracias por registrarte con nosotros. Tu nueva cuenta ha sido configurada y ahora puedes acceder al área de búsqueda y búsquedas avanzadas.
+
+
+</p>
+  <a href="http://grupo2.virtualtic.co/" class="myButton">Ir ha tu cuenta</a>
+</div>
+
+</body>
+</html>
