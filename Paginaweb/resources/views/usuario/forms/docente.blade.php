f48972ca6650825e52046a4c8cc9050b172e365b
constanza becerra tamayo <constanza becerra tamayo>
2016-07-25 04:19:18 -0500

    lo arregle

--- Paginaweb/resources/views/usuario/forms/docente.blade.php
+++ Paginaweb/resources/views/usuario/forms/docente.blade.php
@@ -31,13 +31,17 @@
 @if(isset($verificar))
   <div class="scrollbar areas-aparecer-docente-scroll" id="style-1" style="display:inline-block;">
   
-<div id="areas-aparecer-docente" class="force-overflow" style="display:inline-block; width:100%; height:30px;">
+<div id="areas-aparecer-docente" class="force-overflow" style="display:inline-block; width:100%;">
 
 	@foreach($areas as $area)
 
 		@if(in_array($area->id, $areas_publicacion))
-					<div style="height:1px;display:inline-block;position:relative;" class="alert alert-success"><span style="position:relative;bottom:10px;">{{$area->nombre}}</span><a style="position:absolute;left:90%;top:0%;" href="#" id="area-aparecer-docente{{$area->id}}" class="eliminar-docente close" data-dismiss="alert" aria-label="close">&times;</a></div>
-					 <input type="hidden" name="areas[]" class="areas-eliminar-hidden" id="hidden-docente-{{$area->id}}" value="{{$area->id}}"/>
+		<div class="general-bordes" id="general-bordes-{{$area->id}}" style="position:relative; display:inline-block; width:50%; margin:2px;">
+		{{$area->nombre}}
+		<a style="position:absolute;left:95%;top:0px;" class="eliminar-area" id="eliminar-area-{{$area->id}}">x</a>
+			<input type="hidden" name="areas[]" class="eliminar-hidden" id="eliminar-hidden-{{$area->id}}" value="{{$area->id}}"/>
+		</div>
+				
 			
 				@endif
 	@endforeach
