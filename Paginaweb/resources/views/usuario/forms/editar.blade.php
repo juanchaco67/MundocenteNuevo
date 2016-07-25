40b69c7f1640f45b62401b8ce4dc52755c68b049
Sergio <sepi_147@hotmail.com>
2016-07-24 22:33:02 -0500

    Corrijo edicion de usuarios desde admin

--- Paginaweb/resources/views/usuario/forms/editar.blade.php
+++ Paginaweb/resources/views/usuario/forms/editar.blade.php
@@ -3,13 +3,21 @@
 	<!-- <h1>Editar</h1>-->
 	@if($usuario->idrol == 1)
 	
+	@if(Auth::check() && Auth::user()->idrol == 3)
+		{!!Form::model($usuario, ['route'=>['usuario.update', $usuario->id], 'method'=>'put'])!!}
+	@else
 		{!!Form::model($usuario, ['route'=>['usuario.update', $usuario->id], 'method'=>'put','class'=>'formularioUpdateDocente','name'=>'formularioDocente','onsubmit'=>'return false;'])!!}
+	@endif
 		 <input type="hidden" name="_token" value="{{csrf_token()}}" id="token"/>
 			@include('usuario.forms.docente')
 
 			<div class="modal-footer">
 				<button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
-				{!!Form::submit('Actualizar', ['class'=>'submit-editar-docente btn btn-primary', 'title'=>'Modificar datos de la cuenta'])!!}
+				@if(Auth::check() && Auth::user()->idrol == 3)
+					{!!Form::submit('Actualizar', ['class'=>'btn btn-primary', 'title'=>'Modificar datos de la cuenta'])!!}
+				@else
+					{!!Form::submit('Actualizar', ['class'=>'submit-editar-docente btn btn-primary', 'title'=>'Modificar datos de la cuenta'])!!}
+				@endif
 			</div>
 
 		{!!Form::close()!!}
@@ -19,15 +27,22 @@
 			{!!Form::close()!!} 
 		--}}
 	@elseif($usuario->idrol == 2)	
-
-		{!!Form::model($usuario, ['route'=>['usuario.update', $usuario->id], 'method'=>'put','name'=>'formularioFuncionario','onsubmit'=>'return false;','class'=>'formularioUpdateFuncionario'])!!}
+		@if(Auth::check() && Auth::user()->idrol == 3)
+			{!!Form::model($usuario, ['route'=>['usuario.update', $usuario->id], 'method'=>'put'])!!}
+		@else
+			{!!Form::model($usuario, ['route'=>['usuario.update', $usuario->id], 'method'=>'put','name'=>'formularioFuncionario','onsubmit'=>'return false;','class'=>'formularioUpdateFuncionario'])!!}
+		@endif
 		 <input type="hidden" name="_token" value="{{csrf_token()}}" id="token"/>
 
 			@include('usuario.forms.funcionario')
 
 			<div class="modal-footer">
 				<button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
-				{!!Form::submit('Actualizar', ['class'=>'submit-editar-funcionario btn btn-primary', 'title'=>'Modificar datos de la cuenta'])!!}
+				@if(Auth::check() && Auth::user()->idrol == 3)
+					{!!Form::submit('Actualizar', ['class'=>'btn btn-primary', 'title'=>'Modificar datos de la cuenta'])!!}
+				@else
+					{!!Form::submit('Actualizar', ['class'=>'submit-editar-funcionario btn btn-primary', 'title'=>'Modificar datos de la cuenta'])!!}
+				@endif
 			</div>
 			
 		{!!Form::close()!!}
